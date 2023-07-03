794fe05410b9f8b6d4c50b78834770c4947886f0
del <dineshdel510@gamil.com>
2023-07-03 17:45:24 +0530

    task-5

---
+++ day-5/resume.js
@@ -0,0 +1,43 @@
+let myResume={
+    "basics": {
+      "name": "Dinesh",
+      "email": "dineshdel510@gamil.com",
+      "phone": 6382632677,
+      "degree": "M.Sc",
+      "location": {
+        "address": "West Tambaram",
+        "postalCode": 600030,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+   
+    "education": [
+      {
+        "institution": "SRM University",
+        "department": "IT",
+        "studyType": "fulltime",
+        "batch start year": 2021,
+        "batch end year": 2023
+      }
+    ],
+    "skills": [
+      
+        "name": "javascript",
+        "level": "beginer",
+        ]
+      }
+    
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "javascript,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
