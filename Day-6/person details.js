9c9b174cd551ae0403259f9667bd1135f395bd4f
del <dineshdel510@gamil.com>
2023-07-10 21:08:53 +0530

    person

---
+++ Day-6/person details.js
@@ -0,0 +1,20 @@
+
+class PersonDetails
+  { 
+  constructor()
+  {
+   var a = {Firstname: "Dinesh", Lastname :"Del", age: 23,
+   gender:"male", mob:6326666666, adds:"West Tambaram", 
+   pin: 6000028}
+   
+    console.log(`First Name : ${a.Firstname}`);
+    console.log(`Last Name : ${a.Lastname}`);
+     console.log(`Age is : ${a.age}`);
+       console.log(`Gander : ${a.gender}`);
+        console.log(`Mobile Number : ${a.mob}`);
+        console.log(`Address : ${a.adds}`);
+         console.log(`Pin Code : ${a.pin}`);
+  }
+}
+
+let personal = new PersonDetails();
