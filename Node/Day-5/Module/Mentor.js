eddbc300f4d77b12e71095156d7136ecfc7dc5f9
del <dineshdel510@gamil.com>
2023-09-29 14:22:00 +0530

    node

--- Node/Day-5/Module/Mentor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const studentSchema = new mongoose.schema({
-    name: String,
-    student: [{ type: mongoose.Schema.Type.Object}]
-});
-
-const Mentor = mongoose.model("student", mentor)
-
-module.exports = Mentor;
--- Node/Day-5/server.js
+++ Node/Day-5/server.js
@@ -1,33 +1,96 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-
-
-
-
-
+const Mentor = require("./models/Mentor");
+const Student = require("./models/Student");
 
 const app = express();
 
 app.use(bodyParser.json());
-const PORT = 3000;
 
-const DB_URL =
+const PORT = process.env.PORT;
 
+// url localhost not work use (0.0.0.0)
+// const DB_URL = "mongodb://0.0.0.0:27017/admin";
+const DB_URL = process.env.DB_URL;
+
+//Connect to MongoDB
 mongoose
- .connect(DB_URL, {})
- .then(() => console.log("Connected to MongoDB"))
- .catch((err) => console.log("Could not connect to MongoDB", err));
+  .connect(DB_URL, {})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Could not connect to MongoDB", err));
 
- app.post("/mentor", async (rep,res) => {
-    try{
-        const mentor = new Mentor(rep, bosy);
-        await mentor.save();
-        res.status(201).send(mentor);
-    }
-    catch (error) {
-        res.status(404).send(erroe);
-    }
- });
+app.post("/mentor", async (req, res) => {
+  try {
+    const mentor = new Mentor(req.body);
+    await mentor.save();
+    res.status(201).send(mentor);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
- app.listen(PORT, () => {
-    console.log("Server is running on PORT")
- });
+app.post("/student", async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    await student.save();
+    res.status(201).send(student);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.post("/mentor/:mentorId/assign", async (req, res) => {
+  try {
+    const mentor = await Mentor.findById(req.params.mentorId);
+    const students = await Student.find({ _id: { $in: req.body.students } });
+
+    students.forEach((student) => {
+      student.cMentor = mentor._id;
+      student.save();
+    });
+
+    mentor.students = [
+      ...mentor.students,
+      ...students.map((student) => student._id),
+    ];
+    await mentor.save();
+    res.send(mentor);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.put("/student/:studentId/assignMentor/:mentorId", async (req, res) => {
+  try {
+    const mentor = await Mentor.findById(req.params.mentorId);
+    const student = await Student.findById(req.params.studentId);
+
+    if (student.cMentor) {
+      student.pMentor.push(student.cMentor);
+    }
+
+    student.cMentor = mentor._id;
+    await student.save();
+    res.send(student);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get("/mentor/:mentorId/students", async (req, res) => {
+  try {
+    const mentor = await Mentor.findById(req.params.mentorId).populate(
+      "students"
+    );
+    res.send(mentor);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT", PORT);
+});
