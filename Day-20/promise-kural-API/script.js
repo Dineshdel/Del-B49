91d02948ec94f0969220262443a68e15bbeda17f
del <dineshdel510@gamil.com>
2023-11-21 00:14:01 +0530

    changes

--- Day-20/promise-kural-API/script.js
+++
@@ -1,8 +0,0 @@
-
-function printMessage(){        
-    var req = fetch("https://api-thirukkural.vercel.app/api?num=x")
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
-            document.getElementById("output").innerHTML =data.line1 + data.line2
-        })  }
